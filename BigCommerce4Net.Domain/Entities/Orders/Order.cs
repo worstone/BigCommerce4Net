6802434b7520038d9f3ade0065474e2f993df9db
Dan Moe <danmoe@evolve-creative.com>
2020-06-05 11:32:32 -0500

    Add PaymentStatus value "Pending"

--- BigCommerce4Net.Domain/Entities/Orders/Order.cs
+++ BigCommerce4Net.Domain/Entities/Orders/Order.cs
@@ -277,6 +277,7 @@ namespace BigCommerce4Net.Domain
         /// captured
         /// refunded
         /// partially
+        /// pending
         /// refunded
         /// voided
         /// </summary>
